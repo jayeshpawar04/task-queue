eab98c8ca97ff9890b06da4e39d50513c3dafeef
jayeshpawar04 <jayesh.pawar.004@gmail.com>
2025-07-15 08:08:13 +0530

    changed concurrency to 10

--- src/bullmq/worker.js
+++ src/bullmq/worker.js
@@ -18,7 +18,7 @@ const worker = new Worker(process.env.QUEUE_NAME, async (job) => {
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD,
   },
-  concurrency: 50,
+  concurrency: 10,
   // Optional: concurrency, limiter, etc.
 });
 
