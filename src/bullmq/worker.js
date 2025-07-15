f35bf5c251429245ec6153c99b7a24f4b4edcd2d
jayeshpawar04 <jayesh.pawar.004@gmail.com>
2025-07-15 09:53:43 +0530

    changed worker.js

--- src/bullmq/worker.js
+++ src/bullmq/worker.js
@@ -1,26 +1,34 @@
-const { Worker } = require('bullmq');
-const axios = require('axios');
-require('dotenv').config();
+const { Worker } = require("bullmq");
+const axios = require("axios");
+require("dotenv").config();
 
-const worker = new Worker(process.env.QUEUE_NAME, async (job) => {
-  const { url, payload } = job.data;
-  try {
-    const { headers, body } = payload; // Extract headers and body from payload
-const response = await axios.post(url, body, { headers }); // Include headers in the request
-    console.log('HTTP task completed:', response.data);
-    return response.data; // Optional: store response as job result
-  } catch (err) {
-    console.error('HTTP task failed:', err.message);
-    throw err; // Will trigger retry if attempts > 1
-  }
-}, {
-  connection: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD,
+console.log("Queue Name:", process.env.QUEUE_NAME); // Log the queue name
+const worker = new Worker(
+  process.env.QUEUE_NAME,
+  async (job) => {
+    const { url, payload } = job.data;
+    try {
+const { headers, body: base64Body } = payload;
+
+const buffer = Buffer.from(base64Body, "base64"); // decode back to Buffer
+
+      const response = await axios.post(url, buffer, { headers }); // Include headers in the request
+      return response.data; // Optional: store response as job result
+    } catch (err) {
+      // console.log("error", err);
+      console.error("HTTP task failed:", err.message);
+      throw err; // Will trigger retry if attempts > 1
+    }
   },
-  concurrency: 10,
-  // Optional: concurrency, limiter, etc.
-});
+  {
+    connection: {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD,
+    },
+    concurrency: 10,
+    // Optional: concurrency, limiter, etc.
+  }
+);
 
 module.exports = worker;
